85169577df3deae6234779a1eb1b3aa157b85b24
Viego <themhsalimi@gmail.com>
2022-07-26 17:05:57 +0430

    add set options

--- .config/nvim/lua/shen/set.lua
+++ .config/nvim/lua/shen/set.lua
@@ -10,3 +10,21 @@ vim.opt.expandtab = true
 
 --Error Bell
 vim.opt.errorbells = false
+
+--Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- more space in the neovim command line for displaying messages
+vim.opt.cmdheight = 1
+
+-- highlight all matches on previous search pattern
+vim.opt.hlsearch = true
+
+-- ignore case in search patterns
+vim.opt.ignorecase = true
+
+-- allow the mouse to be used in neovim
+vim.opt.mouse = "a" 
+
+-- highlight the current line
+vim.opt.cursorline = true  
